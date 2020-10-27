2354b9a990da5f3b2ee73ae29382c41b01a8e800
kalyb prince <kalybprince91@gmail.com>
2020-10-26 19:05:15 -1000

    Create underpressure.py
    Speed test!  Double the integer!

---
+++ 8 Kyu/underpressure.py
@@ -0,0 +1,14 @@
+"""
+Code as fast as you can! You need to double the integer and return it.
+
+
+TEST:
+
+Test.assert_equals(double_integer(2), 4);
+
+"""
+
+def double_integer(i):
+    return i + i
+
+print(double_integer(2)) # -> 4
