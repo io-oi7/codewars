9a41937144dbdd66d2c41d9c2456d18b1c02b833
kalyb prince <kalybprince91@gmail.com>
2020-10-27 15:34:24 -1000

    Create returningstring.py
    Make a function that will return a greeting statement that uses an input; your program should return, "Hello,
    <name> how are you doing today?".

---
+++ 8 Kyu/returningstring.py
@@ -0,0 +1,14 @@
+"""
+Make a function that will return a greeting statement 
+that uses an input; your program should return, "Hello, 
+<name> how are you doing today?".
+
+
+TEST:
+
+Test.assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
+"""
+
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
+
