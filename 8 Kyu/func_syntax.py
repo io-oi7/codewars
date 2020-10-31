9e1a8d7ac88902d757064987374164584facce7c
kalyb prince <kalybprince91@gmail.com>
2020-10-31 09:43:31 -1000

    Create func_syntax.py
    A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.

---
+++ 8 Kyu/func_syntax.py
@@ -0,0 +1,16 @@
+"""
+A student was working on a function and made 
+some syntax mistakes while coding. Help them 
+find their mistakes and fix them.
+
+
+TEST:
+
+@test.describe('Example Tests')
+def example_tests():
+    test.assert_equals(main('take ', 'item'), 'take item')
+    test.assert_equals(main('use ', 'sword'), 'use sword')
+"""
+
+def main(verb, noun):
+    return f"{verb}{noun}"
