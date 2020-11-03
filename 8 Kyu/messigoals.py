8799e9e0b7690d5eb5877de76030e45fe5960c5a
Kalyb Prince <kalybprince91@gmail.com>
2020-11-02 16:49:38 -1000

    Add messigoals.py

---
+++ 8 Kyu/messigoals.py
@@ -0,0 +1,14 @@
+"""
+Use variables to find the sum of the goals Messi 
+scored in 3 competitions.
+
+TEST:
+Test.assert_equals(total_goals, 58, 'total goals should equal to 58')
+"""
+
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
