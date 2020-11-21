9ed1af411a784519cd7e40c27ff4e26f15f86f1a
Kalyb Prince <kalybprince91@gmail.com>
2020-11-21 09:23:26 -1000

    added school_paperwork.py and squaring_arg.py

---
+++ 8 Kyu/squaring_arg.py
@@ -0,0 +1,12 @@
+"""
+Now you have to write a function that takes an 
+argument and returns the square of it.
+
+TESTS:
+test.assert_equals(square(2), 4)
+test.assert_equals(square(50), 2500)
+test.assert_equals(square(1), 1)
+"""
+
+def square(n):
+    return n ** 2
